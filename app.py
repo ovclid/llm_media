359e5610edc9fedae204ae9c644f03d5bb66a55c
ovclid <53159064+ovclid@users.noreply.github.com>
2023-12-02 14:31:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,15 @@ Answer the question based only on the following context:
 Answer the question based on the above context: {question}
 """
 
+import streamlit as st
+import pandas as pd
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+
 
     
 # Create CLI.
