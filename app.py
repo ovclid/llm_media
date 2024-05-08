3bc91cf21bb997216ebfc59a8754bab929b9720a
ovclid <53159064+ovclid@users.noreply.github.com>
2024-05-08 15:00:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -51,12 +51,12 @@ def search_url (_data_info, sources):
         if source not in temp:
             temp.append(source)
     sources = temp
-    st.write(sources)
+    #st.write(sources)
   
     for source in sources:
         title = source.replace(f"{MAIN_DATA_PATH}/", "").replace(".txt", "")
         print(title)
-        st.write(title)
+        #st.write(title)
 
         for data in _data_info:
             #if title == data[0]:
