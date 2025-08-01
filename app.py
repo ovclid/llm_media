3f3ea30ccef665774cbab966e5c91022f4c7a3fa
ovclid <53159064+ovclid@users.noreply.github.com>
2025-08-02 05:17:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ def get_conversation_chain(_db, _model, user_question, _press_release_info, _mar
     elif qestion_first == '@':
         st.write("@표시에 따라 주소로 인식하여 처리합니다.")
         pos = convert_address_to_pos(user_question[1:])
-        st.write(f"입력된 주소 좌표 변환(위도, 경도) : {pos}")            
+        st.write(f"[{user_question[1:]}] 좌표 변환 => {pos}")            
         if pos == "":
           st.write("주소 좌표변환 실패하였습니다. 주소를 재확인해 주세요..!!")
         else:
@@ -147,12 +147,14 @@ def get_conversation_chain(_db, _model, user_question, _press_release_info, _mar
                 _df_market.loc[i, "거리"] = math.sqrt( (_df_market.loc[i, "x좌표"] - pos[0])**2 + (_df_market.loc[i, "y좌표"] - pos[1])**2 )
             
             market_nearest = _df_market[_df_market["거리"] == _df_market["거리"].min()]["시장명"].to_string(index=False)
-            st.write( f"어느 시장에도 속하지 않습니다. 다만 가장 가까운 시장은 {market_nearest} 이라 판단됩니다.")
-            st.write(f"상단의 구역도를 클릭하여 재확인하는 것을 추천합니다.")         
+            st.write( f"어느 구역에도 속하지 않습니다. 다만 가장 가까운 곳은 {market_nearest} 이라 판단됩니다.")
+            st.write(f"아래 구역도를 클릭하여 재확인하는 것을 추천합니다.")
+            st.markdown('[충북 전통시장 및 상점가 구역도(지도기반)](https://cbsmba.github.io/onnuri)')
             #st.write(_market_PolygonInfo[market_nearest])
           else:
             st.write(f"{market_in} 안에 위치해 있습니다.")
-            st.write(f"상단의 구역도를 클릭하여 재확인하는 것을 추천합니다.")  
+            st.write(f"아래 구역도를 클릭하여 재확인하는 것을 추천합니다.")
+            st.markdown('[충북 전통시장 및 상점가 구역도(지도기반)](https://cbsmba.github.io/onnuri)')
             st.write(_market_PolygonInfo[market_in])
         response_text = ""  #없으면 에러    
     elif qestion_first == '#':
