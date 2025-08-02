1abb3f3581c2dc33a5ddc5f353e3b7274c9bb87d
ovclid <53159064+ovclid@users.noreply.github.com>
2025-08-02 18:21:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -330,7 +330,7 @@ def start():
             #st.markdown(f"""<span style='font-weight:bold;'>다만 가장 가까운 곳은</span> <span style='color:blue;'>{result['target_market']}</span> 이라 판단되며,""", unsafe_allow_html=True)
         else:
             st.markdown(f"충북지역 전통시장 및 상점가 중에는 <span style='color:red;'>{result['market_in']}</span> 안에 위치해 있는 것으로 추정됩니다.", unsafe_allow_html=True)
-        st.markdown('[구역도(지도기반)](https://cbsmba.github.io/onnuri)를 클릭하여 재확인 하시는 것을 추천드립니다.')
+        st.markdown('[구역도(지도기반)](https://cbsmba.github.io/onnuri)를 클릭하시면 상세하게 확인 가능합니다.')
 
         # Add red marker for input address
         folium.Marker(
