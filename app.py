b3f068f3ed9cd833966624bce08de8d5d0f68895
ovclid <53159064+ovclid@users.noreply.github.com>
2025-08-02 16:46:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -259,6 +259,14 @@ def start():
     _press_release_info = read_press_release_info()
     _market_PolygonInfo, _df_market = get_marketPolygonInfo()
 
+    # Add image to the upper-right corner
+    col1, col2 = st.columns([3, 1])  # Adjust column ratios as needed
+    with col1:
+        st.markdown("")  # Empty placeholder for left column
+    with col2:
+        image_path = "chungbuk_header_logo.svg"  # Replace with your image file path or URL
+        st.image(image_path, width=100)  # Adjust width as needed
+        
     # Streamlit UI
     st.markdown('[충북 전통시장 및 상점가 구역도(지도기반)](https://cbsmba.github.io/onnuri)')
     st.markdown("<br>", unsafe_allow_html=True)
