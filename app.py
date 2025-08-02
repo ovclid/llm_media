b22df158943b8c3a2e6d4b5f70c79a475d546f58
ovclid <53159064+ovclid@users.noreply.github.com>
2025-08-02 17:51:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -326,8 +326,8 @@ def start():
     if result.get("pos"):
         st.write(f"""'{user_question[1:]}' 좌표 변환 : {result['pos']}""")
         if result["market_in"] == "":
-            st.markdown(f"""<span style='color:red; font-weight:bold;'>어느 구역에도 속하지 않는 것으로 추정됩니다.</span>""", unsafe_allow_html=True)
-            st.markdown(f"""<span style='font-weight:bold;'>다만 가장 가까운 곳은</span> <span style='color:blue;'>{result['target_market']}</span> 이라 판단되며,""", unsafe_allow_html=True)
+            st.markdown(f"""<span style='color:red; font-weight:bold;'>어느 구역에도 속하지 않으며,</span> 가장 가까운 곳은 <span style='color:blue;'>{result['target_market']}</span> 이라 판단됩니다.""", unsafe_allow_html=True)
+            #st.markdown(f"""<span style='font-weight:bold;'>다만 가장 가까운 곳은</span> <span style='color:blue;'>{result['target_market']}</span> 이라 판단되며,""", unsafe_allow_html=True)
         else:
             st.markdown(f"<span style='color:red;'>{result['market_in']}</span> 안에 위치해 있는 것으로 추정됩니다.", unsafe_allow_html=True)
         st.markdown('[구역도(지도기반)](https://cbsmba.github.io/onnuri)를 클릭하여 재확인 하시는 것을 추천드립니다.')
