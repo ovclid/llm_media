b3fa5827854b38b9cb9d47dc541930519e77174b
ovclid <53159064+ovclid@users.noreply.github.com>
2025-08-02 16:48:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -260,10 +260,11 @@ def start():
     _market_PolygonInfo, _df_market = get_marketPolygonInfo()
 
     # Add image to the upper-right corner
-    col1, col2 = st.columns([3, 1])  # Adjust column ratios as needed
+    #col1, col2 = st.columns([3, 1])  # Adjust column ratios as needed
+    col1, col2, col3 = st.columns([3, 1, 1])
     with col1:
         st.markdown("")  # Empty placeholder for left column
-    with col2:
+    with col3:
         image_path = "chungbuk_header_logo.svg"  # Replace with your image file path or URL
         st.image(image_path, width=100)  # Adjust width as needed
         
