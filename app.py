c0472cf01f9b1e7e4389573c926a9268b40a13f7
ovclid <53159064+ovclid@users.noreply.github.com>
2025-08-03 12:16:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -267,7 +267,7 @@ def start():
     _market_PolygonInfo, _df_market = get_marketPolygonInfo()
 
     st.markdown(f"""<div style="text-align: right;">
-        <img src="https://raw.githubusercontent.com/ovclid/llm_media/refs/heads/main/chungbuk_header_logo.svg" width="150"><br><br></div>""",
+        <img src="https://raw.githubusercontent.com/ovclid/llm_media/refs/heads/main/chungbuk_header_logo.svg" width="160"><br></div>""",
         unsafe_allow_html=True)
         
     # Streamlit UI
